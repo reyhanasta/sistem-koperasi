dfb94357055a0e87d1e5e3d37dfabe0e271dc264
Reyhan Asta <astareyhan@gmail.com>
2023-09-25 08:51:52 +0700

    cek

--- resources/views/nasabah/_form.blade.php
+++ resources/views/nasabah/_form.blade.php
@@ -3,27 +3,29 @@
         <label for="name">Nama Nasabah</label>
         <input type="name" class="form-control" id="name" name="name" placeholder="Masukan nama pegawai"
             value="{{ $data->name }}" required>
-            @error('ktp')    
+        @error('ktp')
             <div class="text-danger">KTP yang sama sudah di inputkan</div>
-            @enderror
+        @enderror
     </div>
     <div class="form-group">
         <label for="name">Nomor KTP</label>
         <input type="ktp" class="form-control" id="ktp" name="ktp" placeholder="Masukan KTP Nasabah"
             value="{{ $data->ktp }}" required>
-            @error('ktp')    
+        @error('ktp')
             <div class="text-danger">KTP yang sama sudah di inputkan</div>
-            @enderror
+        @enderror
     </div>
-    @if ($data->ktp_image_path)
-        <img src="{{ asset('storage/' . $data->ktp_image_path) }}" alt="Gambar KTP" class="img-thumbnail"
-            style="width: 200px; height: auto;">
-    @else
-        <p>Gambar KTP belum diunggah.</p>
-    @endif
+
 
     <div class="form-group">
         <label for="exampleInputFile">Foto KTP</label> (Opsional)
+        <div class="input-group">
+            @if ($data->ktp_image_path)
+                <img src="{{ asset('storage/' . $data->ktp_image_path) }}" alt="Gambar KTP" class="img-thumbnail"
+                    style="width: 200px; height: auto;">
+            @endif
+        </div>
+
         <div class="input-group">
             <div class="custom-file">
                 <input type="file" class="custom-file-input" id="ktp_image" name="ktp_image">
@@ -50,11 +52,13 @@
     <div class="form-group">
         <label for="gender">Jenis Kelamin</label>
         <div class="form-check">
-            <input class="form-check-input" type="radio" name="gender" value="male" {{ old('gender', $data->gender) === 'male' ? 'checked' : '' }}>
+            <input class="form-check-input" type="radio" name="gender" value="male"
+                {{ old('gender', $data->gender) === 'male' ? 'checked' : '' }}>
             <label class="form-check-label">Pria</label>
         </div>
         <div class="form-check">
-            <input class="form-check-input" type="radio" name="gender" value="female" {{ old('gender', $data->gender) === 'female' ? 'checked' : '' }}>
+            <input class="form-check-input" type="radio" name="gender" value="female"
+                {{ old('gender', $data->gender) === 'female' ? 'checked' : '' }}>
             <label class="form-check-label">Wanita</label>
         </div>
     </div>
